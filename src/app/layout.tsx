c026e661eaf9fb29dee25f298ee39f8ee00a1167
wahyukurnwn <whykurniaawan@gmail.com>
2025-11-25 01:14:26 +0700

    final commit for this day

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
-import { ClerkProvider } from "@clerk/nextjs";
 import { Toaster } from "@/components/ui/sonner";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
